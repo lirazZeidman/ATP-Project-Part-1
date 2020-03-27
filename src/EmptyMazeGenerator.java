b69e5e82eaca98abf3e61ef3571dc745f5aad9af
daniel-izmaylov <58132915+daniel-izmaylov@users.noreply.github.com>
2020-03-27 19:33:56 +0300

    update

--- src/EmptyMazeGenerator.java
+++ src/EmptyMazeGenerator.java
@@ -1,20 +1,12 @@
 public class EmptyMazeGenerator extends  AMazeGenerator {
 
     @Override
-    //TODO we dont need the new class jus change it by Maze.mazetable[1][1]="0";
     public Maze generate(int row, int column) {
         Maze maze= new Maze(row,column);
-        for (int i = 0; i <rowSize ; i++) {
-            for (int j = 0; j <columnSize ; j++) {
-                MazeTable[i][j]=0;
-            }
-        }
-        StartPosition=new Position(0,0);
-        GoalPosition= new Position(rowSize-1,columnSize-1);
-
-    }
-
+        for (int i = 0; i <row ; i++)
+            for (int j = 0; j < column; j++) maze.MazeTable[i][j]="0";
+        maze.StartPosition=new Position(0,0);
+        maze.GoalPosition= new Position(row-1,column-1);
         return maze;
-
     }
 }
