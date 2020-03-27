a33453843d9aea59511d245d0c8c349e4684ab7e
daniel-izmaylov <58132915+daniel-izmaylov@users.noreply.github.com>
2020-03-27 17:58:39 +0300

    |DAnis update

--- src/main.java
+++ src/main.java
@@ -1,2 +1,12 @@
-public class main {
+import java.util.Arrays;
+
+class main {
+    public static void main(String[] args) {
+        int[][] M = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                M[i][j] = 0;
+            }
+        }
+    }
 }
