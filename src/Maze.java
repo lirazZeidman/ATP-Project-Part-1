fc9410ec4b6928b4f4dfad232313d91e16d92b89
daniel-izmaylov <58132915+daniel-izmaylov@users.noreply.github.com>
2020-03-27 19:34:21 +0300

    update

--- src/Maze.java
+++ src/Maze.java
@@ -7,51 +7,35 @@ public class Maze {
     //TODO: change to string and change the print
     protected Position StartPosition;
     protected Position GoalPosition;
+    private int Done;
 
 
+
+    public void setMazeTable(String[][] mazeTable) {
+        MazeTable = mazeTable;
+    }
+
+    public void setStartPosition(Position startPosition) {
+        StartPosition = startPosition;
+    }
+
+    public void setGoalPosition(Position goalPosition) {
+        GoalPosition = goalPosition;
+    }
+
+    public void setDone() {
+        if (Done == 0) {
+            Done = 1;
+        }
+    }
 
     public Maze(int rowSize, int columnSize) {
-        if(rowSize<1 || columnSize <1){
-            //TODO Throw Exceptions
-            return;
-        }
         ColumnSize = columnSize;
         RowSize = rowSize;
         MazeTable = new String[RowSize][ColumnSize];
         StartPosition=new Position(0,0);
         GoalPosition= new Position(rowSize,columnSize);
     }
-
-    public void setMazeTable(String[][] mazeTable) {
-        if (mazeTable==null)
-            return;
-        //TODO Throw Exceptions
-        MazeTable = mazeTable;
-    }
-
-    protected void setStartPosition( int row, int col) {
-        if(row<0||col<0)
-            return;
-        //TODO Throw Exceptions
-        if(row>=RowSize||col>=ColumnSize)
-            return;
-        //TODO Throw Exceptions
-        StartPosition.setPosition(col,row);
-        MazeTable[row][col]="S";
-    }
-
-    protected void setGoalPosition(  int row, int col) {
-        if(row<0||col<0)
-            return;
-        //TODO Throw Exceptions
-        if(row>=RowSize||col>=ColumnSize)
-            return;
-        //TODO Throw Exceptions
-
-        GoalPosition.setPosition(col,row);
-        MazeTable[row][col]="E";
-    }
-
 
     public Position getStartPosition() {
         return StartPosition;
