5eaad112fedd29cff63992f93d4dc81dc9004a80
daniel-izmaylov <58132915+daniel-izmaylov@users.noreply.github.com>
2020-03-27 19:45:04 +0300

    Mase class and Position class update

--- src/SimpleMazeGenerator.java
+++ src/SimpleMazeGenerator.java
@@ -1,6 +1,12 @@
 public class SimpleMazeGenerator extends  AMazeGenerator   {
     @Override
     public Maze generate(int row, int column) {
-        return null;
+        Maze maze= new Maze(row,column);
+        for (int i = 0; i <row ; i++)
+            for (int j = 0; j < column; j++) maze.MazeTable[i][j]="0";
+        maze.StartPosition=new Position(0,0);
+        maze.GoalPosition= new Position(row-1,column-1);
+        return maze;
+    }
     }
 }
