8a2bf403844ed3a40c9c6f13e5e0e4d24e4e3449
daniel-izmaylov <58132915+daniel-izmaylov@users.noreply.github.com>
2020-03-27 20:54:36 +0300

    Merge branch 'master' of C:\Users\danii\Desktop\Dani\Semster 3\ATP-Project-Part-1 with conflicts.

--- src/main.java
+++ src/main.java
@@ -2,14 +2,18 @@ import java.util.Arrays;
 
 class main {
     public static void main(String[] args) {
-        int[][] M = new int[6][6];
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                M[i][j] = 0;
-            }
-        }
+//        int[][] M = new int[6][6];
+//        for (int i = 0; i < 6; i++) {
+//            for (int j = 0; j < 6; j++) {
+//                M[i][j] = 0;
+//            }
+//        }
+//
+//        int[][] v = new int[0][0];
+        SimpleMazeGenerator SMG= new SimpleMazeGenerator();
+        Maze test1= SMG.generate(2,2);
+        test1.Print();
 
-        int[][] v = new int[0][0];
 
 
     }
