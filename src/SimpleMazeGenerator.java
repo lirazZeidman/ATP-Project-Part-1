df780bfcd949f7485094360a3fbb65b7c8507cc9
daniel-izmaylov <58132915+daniel-izmaylov@users.noreply.github.com>
2020-03-28 14:18:12 +0300

    Mase class and Position class update

--- src/SimpleMazeGenerator.java
+++ src/SimpleMazeGenerator.java
@@ -5,15 +5,14 @@ public class SimpleMazeGenerator extends  AMazeGenerator {
     public Maze generate(int row, int column) {
         EmptyMazeGenerator emptymaker= new EmptyMazeGenerator();
         Maze maze = emptymaker.generate(row,column);
-        if (row==1 || column==1)
-            return maze;
-
-        // create instance of Random class
-        Random rand = new Random();
-        // Generate random integers in range 0 to 999
-        int rand_int1 = rand.nextInt(column-1)+1; // -1 beacuse of the entrance +1 so it cant be 0
-        maze.MazeTable[0][rand_int1]=1;
+        if (row != 1 && column != 1) {// create instance of Random class
+            Random rand = new Random();
+            // Generate random integers in range 0 to 999
+            int rand_int1 = rand.nextInt(column - 1) + 1; // -1 beacuse of the entrance +1 so it cant be 0
+            maze.MazeTable[0][rand_int1] = 1;
+        }
         return maze;
+
     }
 }
 
