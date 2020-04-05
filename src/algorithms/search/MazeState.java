569dfab0efdd9929ff9c6cb0b183370abeb0f7a6
liraz zeidman <liraz.zeidman@gmail.com>
2020-04-05 13:32:32 +0300

    Mase class and Position class update

--- src/algorithms/search/MazeState.java
+++ src/algorithms/search/MazeState.java
@@ -5,6 +5,9 @@ import algorithms.mazeGenerators.Position;
 public class MazeState extends AState   {
 
 
+
+
+
     public MazeState(Position curNode, AState prev, int cost) {
         super(curNode.toString(), prev, cost);
     }
